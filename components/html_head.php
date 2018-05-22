7ea86eac8b5915bf02954f108b3a07fddcdb994e
Mark Paspirgilis <mark-paspirgilis@web.de>
2018-05-22 23:21:54 +0200

    robots.txt

--- components/html_head.php
+++ components/html_head.php
@@ -2,6 +2,8 @@
 
 <title>jQueryPlugin.de</title>
 
+<meta name="google-site-verification" content="DqMroh4IVbq3CJ0YalPKxBbHSVe021uY4Dj2jn5i3sA" />
+
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <meta name="theme-color" content="#69D269"/>
